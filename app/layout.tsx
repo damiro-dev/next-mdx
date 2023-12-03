7369d4f125fa16a194f0862eeafcf33287095a28
damiro <hello.damiro@gmail.com>
2023-12-03 16:06:23 +0800

    removed next-themes since its making uncontrollable errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import Link from 'next/link';
 import Script from 'next/script';
 import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/components/theme-provider';
 import { ModeToggle } from '@/components/mode-toggle';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,25 +17,23 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang='en'>
+    <html lang='en' className='light'>
       <Script defer src='https://identity.netlify.com/v1/netlify-identity-widget.js' />
       <body
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <div className='max-w-2xl mx-auto py-10 px-4 prose-quoteless'>
-            <header>
-              <div className='flex items-center justify-between'>
-                <ModeToggle />
-                <nav className='ml-auto text-sm font-medium space-x-6'>
-                  <Link href='/'>Home</Link>
-                  <Link href='/about'>About</Link>
-                </nav>
-              </div>
-            </header>
-            <main>{children}</main>
-          </div>
-        </ThemeProvider>
+        <div className='max-w-2xl mx-auto py-10 px-4 prose-quoteless'>
+          <header>
+            <div className='flex items-center justify-between'>
+              <ModeToggle />
+              <nav className='ml-auto text-sm font-medium space-x-6'>
+                <Link href='/'>Home</Link>
+                <Link href='/about'>About</Link>
+              </nav>
+            </div>
+          </header>
+          <main>{children}</main>
+        </div>
         <Script id='netlify-cms'>
           {`if (window.netlifyIdentity) {
             window.netlifyIdentity.on("init", user => {
